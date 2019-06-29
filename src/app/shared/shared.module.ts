22f16301530ae98cc875c9cb717e4d42a8b54935
Bartosz Jakubowski <bjakubov@gmail.com>
2019-06-29 19:42:50 +0200

    feat: Add dumb sidebar menu component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,13 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { CoreModule } from '../core/core.module';
 import { NavbarMobileMenuComponent } from './navbar/navbar-mobile-menu/navbar-mobile-menu.component';
 import { NavbarDesktopMenuComponent } from './navbar/navbar-desktop-menu/navbar-desktop-menu.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 const components = [
 	NavbarComponent,
 	NavbarMobileMenuComponent,
-	NavbarDesktopMenuComponent
+	NavbarDesktopMenuComponent,
+	SidebarComponent
 ];
 
 @NgModule({
