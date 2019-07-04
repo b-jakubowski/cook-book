4e09e8d774bcd63a1ac7bdbf724234b0d4c94d3d
Bartosz Jakubowski <bartosz.jakubowski@tieto.com>
2019-07-04 18:08:38 +0200

    feat: Added fetch from db dummy method

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root'
@@ -8,6 +8,10 @@ import {Observable} from 'rxjs';
 export class RecipesService {
 
 	constructor(private http: HttpClient) { }
+
+	fetchRecipes(): Observable<any> {
+		return this.http.get('');
+	}
 
 	fetchRecipesFromMock(): Observable<any> {
 		return this.http.get('/assets/mocks/recipes-mock.json');
