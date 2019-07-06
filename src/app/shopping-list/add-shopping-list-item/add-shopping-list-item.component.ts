7b2dd7375d11198886b472b786889c552bfdd149
Bartosz Jakubowski <bartosz.jakubowski@tieto.com>
2019-07-06 12:18:38 +0200

    feat: Added dummy add-shopping-list-item component

---
+++ src/app/shopping-list/add-shopping-list-item/add-shopping-list-item.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-add-shopping-list-item',
+	templateUrl: './add-shopping-list-item.component.html',
+	styleUrls: ['./add-shopping-list-item.component.scss']
+})
+export class AddShoppingListItemComponent {
+}
