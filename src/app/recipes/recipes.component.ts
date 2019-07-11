915167860e837fa5ffd7c6309f63529849d8d3d2
Bartosz Jakubowski <bartosz.jakubowski@tieto.com>
2019-07-11 08:33:00 +0200

    refactor: Delete unnecessary fetching

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Recipe } from './recipe-item/recipe.interface';
-import { RecipesService } from './recipes.service';
 
 @Component({
 	selector: 'app-recipes',
@@ -9,7 +6,5 @@ import { RecipesService } from './recipes.service';
 	styleUrls: ['./recipes.component.scss']
 })
 export class RecipesComponent {
-	recipes: Observable<Recipe[]> = this.recipesService.fetchRecipesFromMock();
 
-	constructor(private recipesService: RecipesService) {}
 }
