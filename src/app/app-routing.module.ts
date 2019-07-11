bf4c1aa64bfb323b1a5659c43cce046c3cf74098
Bartosz Jakubowski <bartosz.jakubowski@tieto.com>
2019-07-11 08:28:13 +0200

    fix: Fixed recipe detail in routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { EditRecipeComponent } from './recipes/edit-recipe/edit-recipe.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import {RecipeListComponent} from './recipes/recipe-list/recipe-list.component';
 
 const appRoutes: Routes = [
 	{ path: '', redirectTo: 'recipes', pathMatch: 'full' },
-	{ path: 'recipes', component: RecipesComponent, children: [
-			{ path: ':id', component: RecipeDetailComponent }
-	]},
+	{ path: 'recipes', component: RecipeListComponent},
+	{ path: 'recipes/:id', component: RecipeDetailComponent },
 	{ path: 'edit-recipe', component: EditRecipeComponent },
 	{ path: 'shopping-list', component: ShoppingListComponent },
 ];
