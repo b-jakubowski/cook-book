97f303668d58bfeabc1ccbf10d679598c0baf317
Bartosz Jakubowski <bjakubov@gmail.com>
2019-07-14 20:35:24 +0200

    feat: Added shopping-list reducer

---
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -0,0 +1,37 @@
+import * as ShoppingListActions from './shopping-list.actions';
+import { Ingredient } from 'src/app/recipes/recipe-item/ingredient.interface';
+
+export interface State {
+	ingredients: Ingredient[];
+}
+
+export type Action = ShoppingListActions.ShoppingListActions;
+
+export function shoppingListReducer(
+	state: State,
+	action: ShoppingListActions.ShoppingListActions
+) {
+	switch (action.type) {
+		case ShoppingListActions.ADD_INGREDIENT:
+			return {
+				...state,
+				ingredients: [...state.ingredients, action.payload]
+			};
+		case ShoppingListActions.ADD_INGREDIENTS:
+			return {
+				...state,
+				ingredients: [...state.ingredients, ...action.payload]
+			};
+		case ShoppingListActions.FETCH_INGREDIENTS:
+			return {
+				...state
+			};
+		case ShoppingListActions.FETCH_INGREDIENTS_SUCCESS:
+			return {
+				...state,
+				ingredients: [...action.payload]
+			};
+		default:
+			return state;
+	}
+}
