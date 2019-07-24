0db7b970164398fd51a7cbf2d7de25a3dc3ab07a
Bartosz Jakubowski <bartosz.jakubowski@tieto.com>
2019-07-24 11:29:05 +0200

    refactor: Changed recipes detail component children behavior

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,28 +1,30 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Route} from '@angular/router';
+import {Component} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 import {Observable} from 'rxjs';
 import { Store } from '@ngrx/store';
 import { Recipe } from '../recipe.interface';
-import { filter } from 'rxjs/operators';
+import {filter} from 'rxjs/operators';
 
 @Component({
 	selector: 'app-recipe-detail',
 	templateUrl: './recipe-detail.component.html',
 	styleUrls: ['./recipe-detail.component.scss']
 })
-export class RecipeDetailComponent implements OnInit {
+export class RecipeDetailComponent {
 	activatedRouteId: string = this.activatedRoute.snapshot.params.id;
-	selectedRecipeImg: Observable<Recipe[]> = this.store.select(state => state.recipes.recipes);
+	selectedRecipe$: Observable<Recipe> = this.store.select(state => state.recipes.entities[this.activatedRouteId]).pipe(filter(Boolean));
+	isIngredientsTabSelected = true;
+	isStepsTabSelected = false;
 
-	constructor(private activatedRoute: ActivatedRoute, private store: Store<{ recipes: {recipes: Recipe[] } }>) {}
+	constructor(private activatedRoute: ActivatedRoute, private store: Store<{ recipes: { entities: Recipe[] } }>) {}
 
-	// getImageSource() {
-		
-	// }
+	onIngredientsTabClick() {
+		this.isIngredientsTabSelected = true;
+		this.isStepsTabSelected = false;
+	}
 
-	ngOnInit() {
-		this.selectedRecipeImg.pipe(filter(Boolean)).subscribe(data => console.log(data));
-		// this.activatedRouteId.subscribe(data => console.log(data));
-		// console.log(this.activatedRoute.snapshot.params.id);
+	onStepsTabClick() {
+		this.isIngredientsTabSelected = false;
+		this.isStepsTabSelected = true;
 	}
 }
