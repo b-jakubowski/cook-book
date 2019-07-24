4193c4e69efccfe41350b72e506e6eb6f59078c6
Bartosz Jakubowski <bartosz.jakubowski@tieto.com>
2019-07-24 11:30:06 +0200

    feat: Provided data to recipe detail ingredients component

--- src/app/recipes/recipe-detail/recipe-detail-ingredients/recipe-detail-ingredients.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail-ingredients/recipe-detail-ingredients.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Recipe } from '../../recipe.interface';
 
 @Component({
-  selector: 'app-recipe-detail-ingredients',
-  templateUrl: './recipe-detail-ingredients.component.html',
-  styleUrls: ['./recipe-detail-ingredients.component.scss']
+	selector: 'app-recipe-detail-ingredients',
+	templateUrl: './recipe-detail-ingredients.component.html',
+	styleUrls: ['./recipe-detail-ingredients.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class RecipeDetailIngredientsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class RecipeDetailIngredientsComponent {
+	@Input() selectedRecipe$: Observable<Recipe>;
 }
