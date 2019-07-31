1ae998105d75f3b32d574487396232ad68014829
Bartosz Jakubowski <bartosz.jakubowski@tieto.com>
2019-07-31 15:19:01 +0200

    feat: Added steps formArray

--- src/app/recipes/edit-recipe/edit-recipe.component.ts
+++ src/app/recipes/edit-recipe/edit-recipe.component.ts
@@ -12,10 +12,13 @@ export class EditRecipeComponent {
 		time: new FormControl(''),
 		kcal: new FormControl(''),
 		imagePath: new FormControl(''),
-		categories: this.formBuilder.array([])
+		categories: this.formBuilder.array([]),
+		steps: this.formBuilder.array([])
 	});
 	categories: FormArray;
+	steps: FormArray;
 	categoryInputVal = '';
+	stepInputVal = '';
 
 	constructor(private formBuilder: FormBuilder) {}
 
@@ -23,21 +26,39 @@ export class EditRecipeComponent {
 		console.log(this.recipeForm.value);
 	}
 
-	addCategory(value: string): void {
-		this.categories = this.recipeForm.get('categories') as FormArray;
-		this.categories.push(
+	pushToFormArray(arr: FormArray, value: string) {
+		arr.push(
 			this.formBuilder.group({
 				name: value
 			})
 		);
+	}
+
+	addCategory(value: string): void {
+		this.categories = this.recipeForm.get('categories') as FormArray;
+		this.pushToFormArray(this.categories, value);
 		this.categoryInputVal = '';
+	}
+
+	addStep(value: string): void {
+		this.steps = this.recipeForm.get('steps') as FormArray;
+		this.pushToFormArray(this.steps, value);
+		this.stepInputVal = '';
 	}
 
 	deleteCategory(key: number): void {
 		this.categories.removeAt(key);
 	}
 
+	deleteStep(key: number): void {
+		this.steps.removeAt(key);
+	}
+
 	onCategoryKey(event: any) {
 		this.categoryInputVal = event.target.value;
+	}
+
+	onStepKey(event: any) {
+		this.stepInputVal = event.target.value;
 	}
 }
