d90f5484c2f702dc5212c3d9447828ae0076b385
Bartosz Jakubowski <bartosz.jakubowski@tieto.com>
2019-08-07 14:45:20 +0200

    feat: Added loading spinner to auth component

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -9,12 +9,17 @@ import { AuthService } from './auth.service';
 })
 export class AuthComponent {
 	isLoginMode = false;
+	isLoading = false;
+	error: string = null;
 
 	constructor(private authService: AuthService) {}
 
-
 	switchMode() {
 		this.isLoginMode = !this.isLoginMode;
+	}
+
+	hideError() {
+		this.error = null;
 	}
 
 	onSubmit(form: NgForm) {
@@ -24,15 +29,20 @@ export class AuthComponent {
 		const email = form.value.email;
 		const password = form.value.password;
 
+		this.isLoading = true;
 		if (this.isLoginMode) {
 			// ...
+			this.isLoading = false;
 		} else {
 			this.authService.signup(email, password).subscribe(
 				resData => {
 					console.log(resData);
+					this.isLoading = false;
 				},
 				error => {
 					console.log(error);
+					this.error = 'an error occured';
+					this.isLoading = false;
 				});
 
 			form.reset();
