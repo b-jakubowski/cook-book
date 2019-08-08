21f8e66e11a8fb353349c27a457a92bf7e45ddde
Bartosz Jakubowski <bartosz.jakubowski@tieto.com>
2019-08-08 11:48:32 +0200

    feat: Added user model

---
+++ src/app/auth/user.model.ts
@@ -0,0 +1,16 @@
+export class User {
+	constructor(
+		public email: string,
+		public id: string,
+		private token: string,
+		private tokenExpirationDate: Date
+	) {}
+
+	get userToken() {
+		if (!this.tokenExpirationDate || new Date() > this.tokenExpirationDate) {
+			return null;
+		}
+
+		return this.token;
+	}
+}
