dd457e87ad8214cf38e645ca23172933ac61f648
Bartosz Jakubowski <bartosz.jakubowski@tieto.com>
2019-08-09 11:04:40 +0200

    refactor: Changed signing in to AngularFire methods

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,89 +1,45 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { BehaviorSubject, Observable, Subject, throwError } from 'rxjs';
-import { AuthResponse } from './auth-response.interface';
+import { from, Observable, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 
-import { environment } from '../../environments/environment';
-import { User } from './user.model';
+import { AngularFireAuth } from '@angular/fire/auth';
+import UserCredential = firebase.auth.UserCredential;
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class AuthService {
-	user = new BehaviorSubject<User>(null);
 
-	constructor(private http: HttpClient) { }
+	constructor(private http: HttpClient, private afAuth: AngularFireAuth, private af: AngularFirestore) { }
 
-	signup(email: string, password: string): Observable<AuthResponse> {
-		return this.http.post<AuthResponse>(
-			'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.userAPIKey,
-			{
-				email,
-				password,
-				returnSecureToken: true
-			}
-		).pipe(
-			catchError(errorRes => this.handleError(errorRes)),
-			tap(resData => this.handleAuth(resData.email, resData.localId, resData.idToken, +resData.expiresIn))
+	signup(email: string, password: string): Observable<any> {
+		return from(this.afAuth.auth.createUserWithEmailAndPassword(email, password)).pipe(
+			catchError(errorRes => this.handleError(errorRes))
 		);
 	}
 
-	login(email: string, password: string): Observable<AuthResponse> {
-		return this.http.post<AuthResponse>(
-			'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.userAPIKey,
-			{
-				email,
-				password,
-				returnSecureToken: true
-			}
-		).pipe(
+	login(email: string, password: string): Observable<UserCredential> {
+		return from(this.afAuth.auth.signInWithEmailAndPassword(email, password)).pipe(
+			tap(() => console.log(this.afAuth.auth.currentUser)),
 			catchError(errorRes => this.handleError(errorRes)),
-			tap(resData => this.handleAuth(resData.email, resData.localId, resData.idToken, +resData.expiresIn))
 		);
+
+	}
+
+	signout() {
+		this.afAuth.auth.signOut();
 	}
 
 	private handleError(errorRes: HttpErrorResponse) {
-		let errorMessage = 'An unknown error occured';
+		const errorMessage = 'An unknown error occured';
 
-		if (!errorRes.error || !errorRes.error.error) {
+		if (!errorRes.message) {
 			return throwError(errorMessage);
 		}
 
-		switch (errorRes.error.error.message) {
-			case 'EMAIL_NOT_FOUND':
-				errorMessage = 'This email does not exist';
-				break;
-			case 'INVALID_PASSWORD':
-				errorMessage = 'Invalid password';
-				break;
-			case 'USER_DISABLED':
-				errorMessage = 'User is disabled';
-				break;
-			case 'EMAIL_EXISTS':
-				errorMessage = 'This email already exists';
-				break;
-			case 'OPERATION_NOT_ALLOWED':
-				errorMessage = 'Operation was not allowed';
-				break;
-			case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-				errorMessage = 'Too many wrong attempts, please try again later';
-				break;
-		}
-
-		return throwError(errorMessage);
+		return throwError(errorRes.message);
 	}
 
-
-	private handleAuth(email: string, userId: string, token: string, expiresIn: number) {
-		const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
-		const user = new User(
-			email,
-			userId,
-			token,
-			expirationDate
-		);
-
-		this.user.next(user);
-	}
 }
